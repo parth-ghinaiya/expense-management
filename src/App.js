bdf7dedf34c7bdc0ed4190cc2d469f1b0dfbe118
Parth Ghinaiya <parth@gopronto.io>
2023-05-26 21:48:00 +0200

    Added first custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Expense management</h1>
+        <ExpenseItem />
       </header>
     </div>
   );
