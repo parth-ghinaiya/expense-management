b67367dcaa33a6a6874fad80dd037af07b9b4f4a
Parth Ghinaiya <parth@gopronto.io>
2023-06-04 19:48:07 +0200

    Add keys to identify all expense as unique

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,6 +6,7 @@ const Expense = (props) => {
     <Card className="expenses">
       {props.items.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
